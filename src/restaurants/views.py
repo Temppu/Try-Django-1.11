f6f034a9cc217bd2675ddf4792c4b7b6c9f51ffd
Teemu WA. Seeve <wa.seevet1@aalto.fi>
2018-01-15 12:27:20 +0200

    adding functionality to the djangop project

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,22 +1,33 @@
+import random
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
 # function based view
 def home(request):
-    html_ = """
-<!doctype html>
-<html lang="en">
-<head>
-</head>
-<body>
-<h1>Hello World!</h1>
-<p>This is html coming through</p>
-</body>
-</html>
-"""
-    
+    num     = None
+    num1    = random.randint(0, 10000)
+    num2    = random.randint(0, 10000)
+    some_list = [random.randint(0, 10000), num1, num2]
 
-    return HttpResponse(html_)
-    #return  render(request, "home.html", {}) # response
+    condition_bool_item = True
+    if condition_bool_item:
+        num     = random.randint(0, 10000)
 
+    context = {"html_var": "context",
+               "num": num,
+               "some_list": some_list
+    }
+
+    return render(request, "home.html", context)
+
+
+def about(request):
+    context = {}
+    return render(request, "about.html", context)
+
+
+def contact(request):
+    context = {}
+    return render(request, "contact.html", context)
